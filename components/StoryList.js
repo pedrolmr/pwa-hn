7753d88d5ae1f8b942a56d81c577a9562cb480cf
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-11-29 20:38:58 -0500

    added styles to the StoryList component

--- components/StoryList.js
+++ components/StoryList.js
@@ -1,8 +1,52 @@
+import Link from 'next/link';
 const StoryList = ({ stories }) => (
-    <div>
+    <div className="story-list">
         {stories.map(story => (
-            <h2 key={story.id}>{story.title}</h2>
+            <div className="story" key={story.id}>
+                <a className="story-title" href={story.url}>{story.title}</a>
+
+                <div className="story-details">
+                    <span>{story.points || 0} points</span>
+                    <Link href={`/story?id=${story.id}`}>
+                        <a>{story.comments_count || 0} comments</a>
+                    </Link>
+                </div>
+            </div>
         ))}
+        <style jsx>{`
+        .story-list{
+
+            padding: 0 1em;
+        }
+        .story {
+            padding:1em 0;
+        }
+        .story-title{
+            font-size: 1rem;
+            font-weight: 400;
+            margin:0;
+            margin-bottom: 0.5em;
+        }
+
+        .story-title a{
+            color:#333;
+            text-decoration: none;
+        }
+        .story-title a:hover, story-details a:hover{
+            text-decoration:underline;
+        }
+        .story-details{
+            font-size:0.8rem;
+            font-weight:bold;
+        }
+        .story-details span{
+            margin-right: 1em;
+        }
+        .story-details a{
+            color:#6600ff;
+            text-decoration:none;
+        }
+        `}</style>
     </div>
 );
 export default StoryList;
