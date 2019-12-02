a2e22a61c53c4a08d8a8627fc2dc8e894dc69f73
pedrolmr <pedroluismontesinosrivera@gmail.com>
2019-12-01 22:39:44 -0500

    added pagination

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,26 @@
 import { Component } from 'react';
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import Link from 'next/link';
 import StoryList from '../components/StoryList';
 import Layout from '../components/Layout';
 
 class Index extends Component{
-    static async getInitialProps(){
+    static async getInitialProps({req, res, query}){
         let stories;
+        let page;
         try {
-            const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+            page = Number(query.page) || 1;
+            const response = await fetch(`https://node-hnapi.herokuapp.com/news?page=${page}`);
             stories = await response.json();
         } catch (error) {
             console.log(error);
             stories = [];
         }
-        return { stories };
+        return { page, stories };
     }
     render(){
-        const { stories } = this.props;
+        const { stories, page } = this.props;
         if( stories.length === 0){
             return <Error statusCode={503} />
         }
@@ -25,6 +28,19 @@ class Index extends Component{
             <Layout title="hacker next" description="hacker news clone made with next.js">
                 <h1>Hackernews</h1>
                 <StoryList stories={stories}/>
+                <footer>
+                    <Link href={`/?page=${page + 1}`}><a>Next page {page + 1}</a></Link>
+                </footer>
+                <style jsx>{`
+                    footer{
+                        padding: 1em;
+                    }
+                    footer a{
+                        font-weight: bold;
+                        color: black;
+                        text-decoration:none;
+                    }
+                `}</style>
             </Layout>
         );
     }
